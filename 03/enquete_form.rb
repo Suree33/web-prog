16212f0896e9df8e9d7651bfe2c9534445dbd839
Daiki Sato <42645594+Suree33@users.noreply.github.com>
2021-06-15 15:59:23 +0900

    commit

--- 03/enquete_form.rb
+++ 03/enquete_form.rb
@@ -3,6 +3,7 @@ require 'cgi'
 cgi = CGI.new
 
 print cgi.header('text/html; charset=utf-8')
+
 print <<~EOF_HTML
   <!DOCTYPE html>
   <html lang="ja">
@@ -20,7 +21,37 @@ print <<~EOF_HTML
       </head>
   
       <body>
-      
+          <header>
+              <h1>投票システム</h1>
+              <div>オーバーウォッチで好きなエスコートマップは？</div>
+          </header>
+          <main>
+              <form action="http://cgi.u.tsukuba.ac.jp/~s1811417/wp/03/vote.rb" , method="post">
+                  <div class="post-form">
+                      <div class="post-form-wrapper">
+                          <ul class="checkbox-list">
+                              <li><input type="checkbox">ドラド</li>
+                              <li><input type="checkbox">ハバナ</li>
+                              <li><input type="checkbox">ジャンカータウン</li>
+                              <li><input type="checkbox">リアルト</li>
+                              <li><input type="checkbox">ルート 66</li>
+                              <li><input type="checkbox">ウォッチポイント・ジブラルタル</li>
+                          </ul>
+                      </div>
+                      <div class="post-form-buttons">
+                          <button class="btn" type="submit">
+                              <i class="fas fa-vote-yea"></i>
+                              投票
+                          </button>
+                          <button class="btn caution" type="reset">
+                              <i class="fas fa-eraser"></i>
+                              クリア
+                          </button>
+                      </div>
+                  </div>
+              </form>
+          </main>
       </body>
+  
   </html>
 EOF_HTML
